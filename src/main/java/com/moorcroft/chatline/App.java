9223594556f16251b015b9ea21b8ead306c3a883
moorcroftg <213037513@mycput.ac.za>
2017-08-06 18:03:48 +0200

    first

---
+++ src/main/java/com/moorcroft/chatline/App.java
@@ -0,0 +1,13 @@
+package com.moorcroft.chatline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
